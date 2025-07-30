1eacdc9dc1c3bd14a318fded6a07b6eeefd99424
Mojtaba Sabbagh <mojtaba.sabbagh@yahoo.com>
2025-07-30 14:01:43 +0330

    Send message Error that was fixed.

--- components/PortalGrid.tsx
+++ components/PortalGrid.tsx
@@ -12,7 +12,7 @@ export default function PortalGrid() {
   ];
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
       {portals.map(({ href, text }) => (
         <a
           key={href}
